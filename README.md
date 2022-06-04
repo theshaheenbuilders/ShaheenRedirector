ea0fce0a20b70aec8130f5804886fc9c5315a0b5
theshaheendevelopers <theshaheendevelopers@gmail.com>
2022-06-05 02:55:50 +0500

    dssf

---
+++ README.md
@@ -0,0 +1,4 @@
+# ShaheenEnclaveExpressApp
+
+
+"# ShaheenEnclaveExpressApp" 
---
+++ server.js
@@ -0,0 +1,18 @@
+var express = require("express");
+var cors = require("cors");
+var bodyParser = require("body-parser");
+
+var app = express();
+const port = process.env.PORT || 5000;
+
+app.use(cors());
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+
+app.use("/", (request, response) => {
+    response.redirect(`http://www.theshaheenbuilders.com/OTPVerification.php?Id=${request.query.Id}`);
+});
+
+app.listen(port, () => {
+    console.log("Server is running on port: " + port)
+})
